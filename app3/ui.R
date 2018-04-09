1ef7247a172ea37bdab9adac1e4c128395aa64c6
Max Galka <galkamaxd@gmail.com>
2018-04-09 16:35:11 -0400

    Update ui.R

--- app3/ui.R
+++ app3/ui.R
@@ -1,5 +1,4 @@
 
-
 library(shiny)
 
 fluidPage(
@@ -12,7 +11,7 @@ fluidPage(
       sliderInput("circlesize", "How big should the points be?", 1, 100, 30),
       sliderInput("textsize", "How big should the text be?", 1, 10, 2),
       selectInput("colorpalette", label = "Select a color palette", 
-                  choices = list("The Accent palette" = "Accent", "Set1 perhaps?" = "Set1", "Maybe Pastel1?" = "Pastel1", "Greens would be a bad choice"="Greens"), 
+                  choices = list("The Accent palette" = "Accent", "The Set1 palette" = "Set1", "The Pastel1 palette" = "Pastel1", "The Greens palette (don't choose this one)"="Greens"), 
                   selected = "Set1")
     ),
     
